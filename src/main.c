6fa499c8299236d042f709a5a24c0cd543000ccc
panagiotou23 <alexandp@ece.auth.gr>
2020-12-15 12:21:45 +0200

    Added main.c

--- src/main.c
+++ src/main.c
@@ -4,67 +4,49 @@
 #include <math.h>
 
 #include "v1.h"
-#include "v2.h"
-// #include "v2.1.h"
-// #include "vp_tree.h"
 
 int main(int argc, char *argv[]){
     
+    //Initialze rand()
     srand(time(0));
 
-    int n = 100;
-    int d = 2;
+    //Set the number and dimensions of the data points
+    int n = 20000;
+    int d = 3;
+    //Set the number of nearest neighbours
     int k = 10;
 
-
+    //Initialize the corpus data points
     double *X = (double *)malloc(n * d * sizeof(double));
-    int *idx = (int *)malloc(n * sizeof(int));
+    for(int i=0; i<n*d; i++) X[i] = (double)(rand()%100000)/1000;
 
-    for(int i=0; i<n*d; i++) {
-        X[i] = (double)(rand()%100000)/1000;
-        idx[i] = i;
-    }
-
-    // for(int i=0; i<n; i++){
-    //     for(int j=0; j<d; j++) printf("%lf ", X[j + i*d]);
-    //     printf("\n");
-    // }
-
-    // vps_node nod = make_vps_tree(X, n, d);
-
-    node nod = make_vp_tree(X, idx, n, d);
-
-    // node nd = make_vp_tree(X, n, d, k);
-    
-    // for(int i=0; i<n; i++){
-
-    //     int id = 0;
-    //     double min = INFINITY;
-    //     search(nod, X + i*d, d, &min, &id);
-
-    //     printf("min: %lf\tid: %d\n",min, id);
-
-    // }
-    return 0;
-
-    //The Variables used to time the function
+    //Declare the variables used to time the function
     struct timespec ts_start;
     struct timespec ts_end;
 
+    //Start the clock
     clock_gettime(CLOCK_MONOTONIC, &ts_start);
+
+    //Find the knn of the corpus data points
     knnresult knn = distrAllkNN(X, n, d, k);
+
+    //Stop the clock
     clock_gettime(CLOCK_MONOTONIC, &ts_end);
 
+    //If the return struct is not empty
     if(knn.k != 0){
+        //Print the knn and their indices
         for(int i=0; i<knn.k; i++){
             printf("Dist: %lf  \tIdx: %d\n", knn.ndist[i], knn.nidx[i]);
         }
         printf("\n\n");
 
+        //Print the time of V1
         long time = (ts_end.tv_sec - ts_start.tv_sec)* 1000000 + (ts_end.tv_nsec - ts_start.tv_nsec)/ 1000;
         printf("Time \n%ld us\n%f s\n", time, (float)time*1e-6);     
     }   
 
+    //Free the corpus data points
     free(X);
     return 0;
 }
